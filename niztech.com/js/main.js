803d66d7be3997ae81e3c5feeddf8f0cbe207149
Nazario A. Ayala <nazario.a@gmail.com>
2015-06-01 13:44:35 -0700

    Mostly altered stuff related to work examples - Added JS for click functionality - Altered the mast size

---
+++ niztech.com/js/main.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+
+  $('form input').on('click', function (){
+    if($(this).attr('name') === 'name' && $(this).val() === 'Peter Parker'){
+      $(this).val('');
+    }else if ($(this).attr('name') === 'email' && $(this).val() === 'webslinger@gmail.com') {
+      $(this).val('');
+    }
+  });
+
+  $('form input').on('blur', function (){
+    if($(this).attr('name') === 'name' && $(this).val().trim() === ''){
+      $(this).val('Peter Parker');
+    }else if ($(this).attr('name') === 'email' && $(this).val().trim() === '') {
+      $(this).val('webslinger@gmail.com');
+    }
+  });
+
+
+  $('#work .subselect li').on('click', function(){
+    var id_to_display = '#' + $(this).attr('class') + '_examples';
+    console.log(id_to_display);
+    $('#work .showcase .examples').addClass('hidden');
+    $(id_to_display).removeClass('hidden');
+  });
+
+});
