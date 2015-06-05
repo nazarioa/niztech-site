7942ff756ac66b54b769f82a97fc343f8514c0c0
Nazario A. Ayala <nazario.a@gmail.com>
2015-06-05 00:14:10 -0700

    Renamed files, added smooth scrolling - I renamed img files to be consistant. - I added a few new IMg files for potential use later - Added smooth scrolling to work section - lowerecasedthe t in tidbits - tried to fix the flex issue with sfaari - renamed chevron file names to be more consistant

--- niztech.com/js/main.js
+++ niztech.com/js/main.js
@@ -1,5 +1,24 @@
 $(document).ready(function(){
+  /* Work examples */
+  $('#work .subselect li').on('click', function(){
+    var id_to_display = '#' + $(this).attr('class') + '_examples';
+    console.log(id_to_display);
 
+    if( $(this).hasClass('active') === true ){
+      $('#work .subselect li').removeClass('active');
+      $('#work .showcase .examples').addClass('hidden');
+      console.log('a');
+    }else{
+      $('#work .subselect li').removeClass('active');
+      $(this).addClass('active');
+      $('#work .showcase .examples').addClass('hidden');
+      $(id_to_display).removeClass('hidden');
+      console.log('b');
+      $.smoothScroll({scrollTarget: id_to_display});
+    }
+  });
+
+  /* Contact Form */
   $('#sendmessage input').on('click', function (){
     if($(this).attr('name') === 'sendername' && $(this).val() === 'Peter Parker'){
       $(this).val('');
@@ -20,28 +39,24 @@ $(document).ready(function(){
     }
   });
 
-
-  $('#work .subselect li').on('click', function(){
-    var id_to_display = '#' + $(this).attr('class') + '_examples';
-    console.log(id_to_display);
-    $('#work .showcase .examples').addClass('hidden');
-    $(id_to_display).removeClass('hidden');
-  });
-
   $('#sendmessage button.send').on('click', function(e){
 
     if( $('#sendername').val() !== '' && $('#sendername').val() !== $('#sendername').attr('value') && $('#senderemail').val() !== $('#senderemail').attr('value') ){
 
+      // when the ajax call finishes we want to put our button's text back the way it was.
       var oldvalue = $('#sendmessage button.send').html();
-      $('#contact .result').html('Sending...');
-      $('#contact .overlay').removeClass('hidden');
-
-      var messageData = $('#sendmessage').serialize();
       $('#sendmessage button.send').html('Sending');
       $('#sendmessage input, #sendmessage textarea').attr('disabled','disabled');
 
+      // prep our overlay message and display it
+      $('#contact .result').html('Sending...');
+      $('#contact .overlay').removeClass('hidden');
 
-      var request = $.ajax("content/sensitive_send.php", { //// context: document.body
+      // Lets prep data for our php code.
+      var messageData = $('#sendmessage').serialize();
+
+      // Let the ajax begin.
+      var request = $.ajax("content/sensitive_send.php", {
         async: true,
         dataType: "json",
         data: messageData,
@@ -57,12 +72,15 @@ $(document).ready(function(){
         $('#contact .result').html('Your message was sent!');
       });
     }else{
+      // oh no! it failed
       $('#sendmessage input').addClass('error');
     }
 
+    // lets prevent the page from relaoding
     e.preventDefault();
   });
 
+  /* contact Form Completion Box */
   $('#contact .close, #contact .overlay').on('click', function(){
     $('#contact .result').html('');
     $('#contact .overlay').addClass('hidden');
