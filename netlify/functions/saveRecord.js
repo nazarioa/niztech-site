4e0231836a19cd5a1a5110441dfdeeb3a99d98c2
Nazario A. Ayala <nazario.a@gmail.com>
2022-01-28 23:03:36 -0800

    Exp lamda functions (#13)

---
+++ netlify/functions/saveRecord.js
@@ -0,0 +1,48 @@
+const {GoogleSpreadsheet} = require('google-spreadsheet');
+
+const getSpreadsheet = async (sheetId, email, privateKey) => {
+  const doc = new GoogleSpreadsheet(sheetId);
+  await doc.useServiceAccountAuth({
+    client_email: email,
+    private_key: privateKey,
+  });
+  return doc;
+};
+
+exports.handler = async function (event, context, callback) {
+  // validate input
+  // authengticate sheet
+  // store results
+
+  let doc;
+  try {
+    doc = getSpreadsheet(process.env.GOOGLE_CONTACTSHEET_ID, process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL, process.env.GOOGLE_PRIVATE_KEY);
+  } catch (err) {
+    callback({statusCode: 500, body: err.toString()});
+    return {statusCode: 500, body: err.toString()};
+  }
+
+  let data;
+  try {
+    data = JSON.parse(event?.body ?? {});
+  } catch (err) {
+    callback({statusCode: 500, body: err.toString()});
+    return {statusCode: 500, body: err.toString()};
+  }
+
+  const newEntry = {
+    Name: data?.name ?? 'Petter',
+    Email: data?.email ?? 'person@gmail.com',
+    Message: data?.message ?? 'hello! How are you?!'
+  };
+
+  try {
+    const sheet = await doc.sheetsByIndex[0];
+    sheet.addRow(newEntry);
+  } catch (err) {
+    callback({statusCode: 500, body: err.toString()});
+    return {statusCode: 500, body: err.toString()};
+  }
+  callback({statusCode: 200, body: 'All is gravy' + JSON.stringify(newEntry)});
+};
+
